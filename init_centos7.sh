0fa4cc7a487cd2dbbb979336db2c573bd0b7315a
yufeichen <euphie.chen@igg.com>
2019-07-23 10:14:44 +0800

    add kernel section

--- init_centos7.sh
+++ init_centos7.sh
@@ -173,6 +173,8 @@ update_kernel()
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
     rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
     yum --enablerepo=elrepo-kernel install kernel-ml -y
+    sed -i '/GRUB_DEFAULT=/s#saved#0#' /etc/default/grub
+    grub2-mkconfig -o /boot/grub2/grub.cfg
 }
 
 install_docker()
