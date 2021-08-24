79436a415acef6ccb33f24edd10500a5df06dd11
Euphie <euphie@vip.qq.com>
2021-08-24 21:41:06 +0800

    Update init_centos7.sh

--- init_centos7.sh
+++ init_centos7.sh
@@ -195,7 +195,7 @@ EOF
 
 install_docker_compose()
 {
-    wget https://get.daocloud.io/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    wget https://get.daocloud.io/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 }
 
